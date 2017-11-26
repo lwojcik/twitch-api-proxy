0bb483a9b994bcfac120aad877808aa36230c2ea
Lukasz Wojcik <kontakt@lukaszwojcik.net>
2017-11-26 01:39:59 +0100

    removing bnet config

--- config.js
+++ config.js
@@ -59,41 +59,4 @@ config.twitch.hostApi.clientId          = env.SG_API_TWITCH_CLIENT_ID;
 
 ////////////////////////////////////////////////////////////////////////////////
 
-config.battlenet = {};
-
-config.battlenet.api = {};
-config.battlenet.api.protocol           = 'https';
-config.battlenet.api.url                = 'api.battle.net';
-config.battlenet.api.port               = 443;
-config.battlenet.api.servers            = [ 'eu', 'us', 'kr', 'tw', 'sea' ];
-
-config.battlenet.oauth = {}
-
-config.battlenet.oauth.api = {}
-config.battlenet.oauth.api.url                  = 'battle.net';
-config.battlenet.oauth.api.urlCn                = 'https://battlenet.com.cn';
-
-config.battlenet.oauth.checkAccessTokenUri      = 'battle.net/oauth/check_token?token=';
-config.battlenet.oauth.checkAccessUriCn         = 'https://battlenet.com.cn/oauth/check_token?token=';
-config.battlenet.oauth.getAccessTokenUri        = 'battle.net/oauth/token';
-config.battlenet.oauth.getAccessTokenUriCn      = 'https://www.battlenet.com.cn/oauth/token';
-config.battlenet.oauth.callbackUri              = config.app.siteUrl + '/callback';
-
-config.battlenet.api.key                        = env.SG_API_BATTLENET_KEY;
-config.battlenet.api.secret                     = env.SG_API_BATTLENET_SECRET;
-
-////////////////////////////////////////////////////////////////////////////////
-
-config.sc2 = {}
-
-config.sc2.expansions                           = [ 'WOL', 'HOTS', 'LOTV'];
-
-config.sc2.races                                = [ 'ALL', 'RANDOM', 'TERRAN', 'ZERG', 'PROTOSS'];
-
-config.sc2.matchMaking = {}
-config.sc2.matchMaking.modes                    = [ 'ALL', '1V1', '2V2', '3V3', '4V4' ];
-config.sc2.matchMaking.queues                   = [ 'ALL', 'LOTV_SOLO', 'LOTV_TWOS', 'LOTV_THREES', 'LOTV_FOURS' ];
-
-////////////////////////////////////////////////////////////////////////////////
-
 module.exports = config;
